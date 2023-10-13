6eee9544c049d294a2fe9a13ec4b19f12cb68c97
Tameem <tameem.handb@gmail.com>
2023-10-14 00:29:54 +0600

    ShopManagement

---
+++ ShopManagment/src/shopmanagment/ShopManagment.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package shopmanagment;
+
+/**
+ *
+ * @author Tameem
+ */
+public class ShopManagment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        login l = new login();
+        l.setVisible(true);
+        l.setLocationRelativeTo(null);
+
+    }
+
+}
